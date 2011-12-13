15c09ebfea86c9a810905fe0ae1399321997edc5
Peter Danenberg <pcd@roxygen.org>
2011-12-13 12:07:09 -0800

    Work in progress

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject reader-macros "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [lambda "1.0.1-SNAPSHOT"]
+                 [cadr "1.0.1-SNAPSHOT"]]
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]
-                     [add-classpath "1.0.2-SNAPSHOT"]])
+                     [add-classpath "1.0.3-SNAPSHOT"]])
