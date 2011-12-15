521cfe441f66ce27b4dd7bdab00ff544d8b71e6f
Peter Danenberg <pcd@roxygen.org>
2011-12-15 10:24:48 -0800

    Last shot at programmatic readers for new.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [lambda "1.0.2-SNAPSHOT"]
-                 [cadr "1.0.1-SNAPSHOT"]]
+                 [cadr "1.0.1-SNAPSHOT"]
+                 [useful "0.7.5"]]
   :dev-dependencies [[debug "1.0.0-SNAPSHOT"]
                      [add-classpath "1.0.3-SNAPSHOT"]])
