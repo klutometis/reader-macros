07b93773c88122bc833f3e39e0e0ee7c16c2b013
TristeFigure <clement.malliet@kisskissbankbank.com>
2020-04-16 15:10:32 +0200

    Bump version to 1.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reader-macros "1.0.2"
+(defproject reader-macros "1.0.3"
   :description "Common Lisp style reader macros"
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :url "https://github.com/klutometis/reader-macros"
